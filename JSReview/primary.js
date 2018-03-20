ab1666790a88cd66d9169cc9de4564e808c9b54f
RubickCraft <429655198@qq.com>
2018-03-20 12:20:56 +0800

    高阶函数

--- JSReview/primary.js
+++ JSReview/primary.js
@@ -1,9 +1,8 @@
-
 {
     !function Loop() {
         let _i = 0, str = '';
 
-        while (_i < 7){
+        while (_i < 7) {
             str += '#';
             console.log(str);
             _i++;
@@ -15,9 +14,9 @@
     !function fizzBuzz() {
         let i = 1;
 
-        while (i < 100){
-            if(i % 3 === 0) {
-                if(i % 5 === 0) {
+        while (i < 100) {
+            if (i % 3 === 0) {
+                if (i % 5 === 0) {
                     console.log('FizzBuzz');
                 }
                 else {
@@ -27,7 +26,7 @@
                 continue;
             }
 
-            if(i % 5 === 0 ) {
+            if (i % 5 === 0) {
                 console.log('Buzz');
                 i++;
                 continue;
@@ -40,7 +39,7 @@
 
 {
     function transferArgs(f) {
-        return function() {
+        return function () {
             return f.apply(null, arguments);
         }
     }
@@ -49,7 +48,7 @@
         console.log(...args);
     }
 
-    transferArgs(con)(1,2,3,4,5);
+    transferArgs(con)(1, 2, 3, 4, 5);
 }
 
 {
@@ -57,6 +56,53 @@
         console.log(args);
     }
 
-    let new_func = myBind.bind(null, 1, 2,3,4);
+    let new_func = myBind.bind(null, 1, 2, 3, 4);
     new_func();
+}
+
+{
+    // 定义数字0:
+    let zero = function (f) {
+        return function (x) {
+            return x;
+        }
+    };
+
+    // 定义数字1:
+    let one = function (f) {
+        return function (x) {
+            return f(x);
+        }
+    };
+
+    // 定义加法:
+    function add(n, m) {
+        return function (f) {
+            return function (x) {
+                return m(f)(n(f)(x));
+            }
+        }
+    }
+
+    // 计算数字2 = 1 + 1:
+    let two = add(one, one);
+
+    // 计算数字3 = 1 + 2:
+    let three = add(one, two);
+
+    // 计算数字5 = 2 + 3:
+    let five = add(two, three);
+
+    // 你说它是3就是3，你说它是5就是5，你怎么证明？
+
+    // 呵呵，看这里:
+
+    // 给3传一个函数,会打印3次:
+
+    let dd = three(function () {
+        console.log(`打印三次---${Math.random()}`);
+    });
+
+    dd();
+
 }
